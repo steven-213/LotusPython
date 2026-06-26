0159f8ca946309cca7b1c2046725a9e085b32bc6
Steven21 <stevengarcia012007@gmail.com>
2026-06-26 10:16:58 -0500

    Update settings.py

--- spa_project/config/settings.py
+++ spa_project/config/settings.py
@@ -203,9 +203,14 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Almacenamiento optimizado para WhiteNoise
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
